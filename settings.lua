d5444a07061657918fdd0e0419e0fee75a9ab7a2
Tom Burrows <tomburrows13@icloud.com>
2021-03-23 22:47:15 +0000

    Remove no longer needed SpidertronEnhancements setting compatibility (moved into that mod)

--- settings.lua
+++ settings.lua
@@ -67,9 +67,3 @@ data:extend({
         order = "cb"
     }
 })
-
--- Disable Spidertron Enhancement features that aren't compatible
-data.raw["bool-setting"]["spidertron-enhancements-enter-entity-base-game"].hidden = true
-data.raw["bool-setting"]["spidertron-enhancements-enter-entity-custom"].hidden = true
-data.raw["bool-setting"]["spidertron-enhancements-show-spider-on-vehicle"].hidden = true
-data.raw["bool-setting"]["spidertron-enhancements-enter-player"].hidden = true
